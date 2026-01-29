d684a5ba3da1d88cc114b24f8d2d65f4c8197c8c
Reshma Ariwala <reshma.ariwala@kombee.com>
2026-01-29 11:53:39 +0530

    Project setup

---
+++ src/webparts/sfDemo/components/ISfDemoProps.ts
@@ -0,0 +1,7 @@
+export interface ISfDemoProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/sfDemo/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ISfDemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'SfDemoWebPartStrings' {
+  const strings: ISfDemoWebPartStrings;
+  export = strings;
+}
